183fce6780743dc8ef8c6bf293b1d1d5de953746
FotiosSkouvas <67700381+FotiosSkouvas@users.noreply.github.com>
2021-11-21 22:28:00 +0200

    Update linear_regression_2.py

--- linear_regression_2.py
+++ linear_regression_2.py
@@ -91,19 +91,18 @@ with dataset:
                     pred_data_feat = pred_data_root[feat].value_counts()
                     st.write(pred_data_feat)
 
-                #Data modeling
+                #Data modelinγ
                 with data_modeling:
                     st.header('Model Training')
-                    st.markdown('We decided to use linear regression for a selected failure and a selected possible cause. We used only the data that fails, in order to achieve better performance of the model. You will find below the prediction and the evaluation of the model:')
                     x_train, x_test, y_train, y_test = train_test_split(pred_data_root[failure], pred_data_root[feat], test_size = 0.2)
                     regr = LinearRegression()
                     regr.fit(np.array(x_train).reshape(-1,1), y_train) #Reshape transverses it from a single dimension matrix to a vertical shape
                     preds = regr.predict(np.array(x_test).reshape(-1,1))
-                    st.write('The expected mean value of possible cause is ', regr.intercept_)
+                    st.write('The expected mean value of feature is ', regr.intercept_)
                     y_pred = regr.predict(np.array(x_test).reshape(-1,1))
 
                     #Evaluation of model
-                    st.markdown('*Evaluation of the model:*')
+                    st.markdown('Evaluation of the model')
                     st.write('Mean Absolute Error:', metrics.mean_absolute_error(y_test, y_pred))
                     st.write('Mean Squared Error:', metrics.mean_squared_error(y_test, y_pred))
                     st.write('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
